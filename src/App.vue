7a8530c0227075e6431d1b6def47618b3660bbe6
Marilisa Moio <moiomarilisa@gmail.com>
2024-07-02 18:28:37 +0200

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script>
-  import HelloWorld from './components/HelloWorld.vue';
+  import AppHeader from './components/AppHeader.vue';
 
   export default {
     components: {
-      HelloWorld,
+      AppHeader,
     }
   }
 </script>
 
 <template>
-  <HelloWorld></HelloWorld>
+  <AppHeader></AppHeader>
   <main>
-    
+
   </main>
 </template>
 

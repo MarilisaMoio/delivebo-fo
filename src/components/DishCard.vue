e0e516a1d5dc5485c22279eda3ee16f5a5ec829f
Marilisa Moio <moiomarilisa@gmail.com>
2024-07-03 13:05:02 +0200

    chore: add other mods

--- src/components/DishCard.vue
+++ src/components/DishCard.vue
@@ -1,12 +1,23 @@
 <script>
 export default {
   name: 'DishCard',
+  props: {
+    dish: Object
+  }
 }
 </script>
 
 <template>
 
-    <h1>SINGOLA CARD</h1>
+    <div class="card" style="width: 18rem;">
+      <img v-if="dish.dish_photo !== null" :src="`http://127.0.0.1:8000/storage/${dish.dish_photo}`" class="card-img-top" alt="...">
+      <img v-else src="../../public/defaultDishes.svg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">{{dish.dish_name}} {{ dish.restaurant_id }}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <btn @click="getPhoto(dish.dish_photo)" class="btn btn-primary">Go somewhere</btn>
+      </div>
+    </div>
  
 </template>
 
---
+++ src/pages/AppDishes.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from 'axios';
+import DishCard from "../components/DishCard.vue"
+
+export default {
+  name: 'AppDishes',
+  components: {
+    DishCard,
+  },
+  data() {
+    return {
+      dishes: [],
+      dishesUrl: ""
+    };
+  },
+  methods: {
+    getDishes() {
+      axios.get('http://127.0.0.1:8000/api/dishes?restaurant=14')
+        .then((response) => {
+          this.dishes = response.data.results;
+        })     
+    }
+  },
+  mounted() {
+    this.getDishes();
+  }
+};
+</script>
+
+<template>
+    <section>
+      <DishCard v-for="dish in dishes" :dish="dish"></DishCard>
+    </section>
+</template>
+
+<style scoped lang="scss">
+</style>
+
+

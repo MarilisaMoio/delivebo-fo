5eb58727d17d45d845c25192f31347b8717cc0b4
simone-danzeri <s.danzeri@icloud.com>
2024-07-04 19:22:15 +0200

    feat: finish landing page

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -7,16 +7,28 @@
 <template>
     <section class="hero d-flex">
         <div class="pippo text-center">
-            <h1 class="text-white ms-margin">Qualcosa tipo non lo so</h1>
+            <h1 class="text-white ms-margin fw-bold ms-main-title">Il Bello è Prenderci Gusto!</h1>
             <button class="ms-btn button">  
-                <router-link class="ms-decoration-none text-white" :to="{ name: 'restaurants'}">Ecco qui tutti i migliori ristoranti di Rovello Porro!</router-link>
+                <router-link class="ms-decoration-none text-white" :to="{ name: 'restaurants'}"><span class="fs-6 fw-light">Ecco qui i migliori ristoranti di Rovello Porro!</span></router-link>
             </button>
         </div>
     </section>
-    <div class="container ms-bg p-0 ">
-        <section class="map">
-            <div>
-                
+    <div class="container p-0">
+        <section class="map ms-bg d-flex">
+            <div class="text-container d-flex d-lg-block align-items-center px-4 py-0">
+                <h4 class="d-none d-lg-block pt-4">Consegne sempre disponibili nella tua città</h4>
+                <p class="my-2 my-md-3 my-lg-4 d-none d-lg-block my-xl-5">Segui in diretta dove si trova il tuo ordine</p>
+                <button class="ms-btn mt-xxl-5">
+                    <router-link class="ms-decoration-none text-white" :to="{ name: 'restaurants'}">Ordina qui dal tuo ristorante preferito!</router-link>
+                </button>
+            </div>
+            <div class="img-container">
+                <div class="message">
+                    <span class="d-flex align-items-center fw-light ms-fontsize"><img src="../../public/favicon.ico" alt="" class="me-1">DELIVEBOO</span>
+                    <p class="fw-bold mb-0">Aggiornamento ordine</p>
+                    <p class="mb-0">Segui da vicino il tuo ordine!</p>
+                </div>
+                <img src="../assets/official-map-rovello.png" alt="">
             </div>
         </section>
     </div>
@@ -27,9 +39,12 @@
 
 <style scoped lang="scss">
    @use '../style/partials/variables' as *;
+   .ms-main-title{
+    font-size: 50px;
+   }
    .hero{
         background-image: url('../assets/wallpaperflare.com_wallpaper.jpg');
-        height: 750px;
+        height: calc(100vh - $header-h);
         width: 100%;
         background-size: cover;
         background-position: bottom;
@@ -41,9 +56,6 @@
     top: 15%;
     left: 50%;
     transform: translateX(-50%);
-   }
-   .ms-margin{
-    margin-bottom: 150px;
    }
    .button{
        transition: 0.4s;
@@ -61,16 +73,57 @@
     box-shadow: 0px 14px 35px 33px rgba(0,0,0,0.05);
     -webkit-box-shadow: 0px 14px 35px 33px rgba(0,0,0,0.05);
     -moz-box-shadow: 0px 14px 35px 33px rgba(0,0,0,0.05);
-       .map{
-            background-image: url('../assets/maps-rovello.png');
-            height: 500px;
-            width: 100%;
-            background-size: cover;
-            background-position: center;
-            position: relative;
-       }
-   }
-   .ms-decoration-none{
-    text-decoration: none;
-   }
+    border-radius: 30px;
+}
+.map{
+ margin: 100px 0;
+ overflow: hidden;
+}
+.ms-margin{
+    margin-bottom: 170px;
+}
+.ms-decoration-none{
+text-decoration: none;
+}
+.img-container{
+width: 50%;
+position: relative;
+img{
+    width: 100%;
+    height: 100%;
+}
+}
+.text-container{
+width: 50%;
+/* padding-top: 4vw;
+padding-left: 4vw; */
+h3{
+    font-size: 3vw;
+}
+p{
+    width: 70%;
+}
+}
+.message{
+    position: absolute;
+    top: 5%;
+    right: 10%;
+    z-index: 10;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0px 14px 35px 33px rgba(0,0,0,0.05);
+    -webkit-box-shadow: 0px 14px 35px 33px rgba(0,0,0,0.05);
+    -moz-box-shadow: 0px 14px 35px 33px rgba(0,0,0,0.05);
+    padding: 1vw 1.2vw;
+    max-width: 80%;
+    p{
+        font-size: 1.3vh;
+    }
+    img{
+        width: 15px;
+    }
+    .ms-fontsize{
+        font-size: 11px;
+    }
+}
 </style>
